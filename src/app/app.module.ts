ddd50e61013083bedb0414128cb6ee3778721de7
Erik Badger <erikbadger777@gmail.com>
2018-09-30 11:01:18 -0500

    Removed dependency from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import {AgmCoreModule} from '@agm/core';
 import { AppComponent } from './app.component';
 import { PitstopComponent } from './components/pitstop/pitstop.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AdderComponent } from './components/adder/adder.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -35,7 +34,6 @@ const appRoutes: Routes = [
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBl8LKxRF1gdmnZjW5NHf3DEHcGCa7AVzY',
     }),
-    NgbModule,
     BrowserAnimationsModule,
   ],
   providers: [],
