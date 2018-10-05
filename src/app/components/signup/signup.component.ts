c80b19bcc59f9b01b0223aa91623874a43d2ffee
Erik Badger <erikbadger777@gmail.com>
2018-10-04 21:28:50 -0500

    Added signup page

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../interfaces/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.less']
 })
 export class SignupComponent implements OnInit {
+  private email;
+  private username;
+  private password;
+  private confirmPassword;
+  private user: User;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onSignupSubmit() {
+    this.user = {
+      email: this.email,
+      username: this.username,
+      password: this.password,
+    };
+    // TODO: Add confirmation service to make sure this is all gucci information were sending back
+    this.authService.registerUser(this.user).subscribe((data: any) => {
+      if (data.success) {
+        this.authService.storeUserData(data.token, data.user);
+        this.router.navigate(['login']);
+      } else {
+        this.router.navigate(['signup']);
+      }
+    });
   }
 
 }
--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -1,10 +1,5 @@
 export interface User {
-    first_name: String;
-    last_name: String;
     email: String;
     username: String;
     password: String;
-    age: Number;
-    display_age: Number;
-    posts: Array<any>;
 }
