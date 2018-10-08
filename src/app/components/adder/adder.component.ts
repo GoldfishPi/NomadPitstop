33defb3878edd73a0d30a611e710715d0d762fc0
Erik Badger <erikbadger777@gmail.com>
2018-10-07 20:43:42 -0500

    Pitstop adding forum

--- src/app/components/adder/adder.component.ts
+++ src/app/components/adder/adder.component.ts
@@ -3,48 +3,85 @@ import { Router } from '@angular/router';
 import { Point } from '@agm/core/services/google-maps-types';
 
 @Component({
-    selector: 'app-adder',
-    templateUrl: './adder.component.html',
-    styleUrls: ['./adder.component.less']
+  selector: 'app-adder',
+  templateUrl: './adder.component.html',
+  styleUrls: ['./adder.component.less']
 })
 export class AdderComponent implements OnInit {
+  modifier = '';
+  adderIcon = '+';
+  open = false;
+  loggedIn = true;
+  expanded = false;
 
-    modifier = '';
-    adderIcon = '+';
-    open = false;
-    loggedIn = true;
-    expanded = false;
+  internetSpeed = 3;
+  internetValue = 'Good';
 
-    @Output() adding: EventEmitter<Boolean> = new EventEmitter();
-    @Input() point: Point;
+  @Output()
+  adding: EventEmitter<Boolean> = new EventEmitter();
+  @Input()
+  point: Point;
 
-    constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
 
-    toggleAdder(event) {
-        if (this.open) {
-            this.modifier = '';
-            this.adderIcon = '+';
-            this.open = false;
-            this.adding.emit(false);
-        } else if (!this.open) {
-            this.modifier = 'open';
-            this.adderIcon = '-';
-            this.open = true;
-            this.adding.emit(true);
-        }
-        this.expanded = false;
+  toggleAdder(event) {
+    if (this.open) {
+      this.modifier = '';
+      this.adderIcon = '+';
+      this.open = false;
+      this.adding.emit(false);
+    } else if (!this.open) {
+      this.modifier = 'open';
+      this.adderIcon = '-';
+      this.open = true;
+      this.adding.emit(true);
     }
-    expandAdder(event) {
-        this.modifier = 'open expand';
-        this.expanded = true;
+    this.expanded = false;
+  }
+  expandAdder(event) {
+    this.modifier = 'open expand';
+    this.expanded = true;
+  }
+  onLogin() {
+    this.router.navigate(['/login']);
+  }
+  onSignup() {
+    this.router.navigate(['/signup']);
+  }
+  onInternetSpeedChange(event) {
+    console.log(event.target.value);
+    this.internetValue = this.internetWords(event.target.value);
+    console.log(this.internetValue);
+  }
+  internetWords(val) {
+    let out: string;
+    switch (val) {
+      case '0':
+        out = 'Awful';
+        break;
+
+      case '1':
+        out = 'Very Bad';
+        break;
+
+      case '2':
+        out = 'bad';
+        break;
+
+      case '3':
+        out = 'Good';
+        break;
+
+      case '4':
+        out = 'very good';
+        break;
+      default:
+        out = 'Excellent';
+        break;
     }
-    onLogin() {
-        this.router.navigate(['/login']);
-    }
-    onSignup() {
-        this.router.navigate(['/signup']);
-    }
+    return out;
+  }
 }
